c315dd51232e34f2e6a29875ba8f09e7125f2456
May Masasa <may.masasa@live.biu.ac.il>
2022-05-23 02:18:26 +0300

    Add project files.

---
+++ ex2/Migrations/20220522220323_Init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ex2.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Stars = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
---
+++ ex2/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace ex2.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Stars { get; set; }
+        public string Comment { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
